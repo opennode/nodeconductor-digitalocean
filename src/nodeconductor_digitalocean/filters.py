32d4dac162df8c397a2f0049a7df10acda4e04e1
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2017-01-23 10:28:24 +0200

    Migrate to DRF 3.5
    - NC-1542

--- src/nodeconductor_digitalocean/filters.py
+++ src/nodeconductor_digitalocean/filters.py
@@ -1,3 +1,5 @@
+from django_filters import OrderingFilter
+
 from nodeconductor.structure import filters as structure_filters
 
 from . import models
@@ -5,16 +7,11 @@ from . import models
 
 class ImageFilter(structure_filters.BaseServicePropertyFilter):
 
+    o = OrderingFilter(fields=('distribution', 'type'))
+
     class Meta(object):
         model = models.Image
         fields = structure_filters.BaseServicePropertyFilter.Meta.fields + ('distribution', 'type')
-        order_by = (
-            'distribution',
-            'type',
-            # desc
-            '-distribution',
-            '-type',
-        )
 
 
 class SizeFilter(structure_filters.BaseServicePropertyFilter):
