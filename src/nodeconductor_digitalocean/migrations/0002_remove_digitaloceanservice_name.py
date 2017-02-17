e444abcbb299063c8e1c22072a84ecc0959c1ff9
tarasmatsyk <tamatsyk@gmail.com>
2017-02-17 16:58:02 +0200

    Fix migration dependencies [WAL-496]

--- src/nodeconductor_digitalocean/migrations/0002_remove_digitaloceanservice_name.py
+++ src/nodeconductor_digitalocean/migrations/0002_remove_digitaloceanservice_name.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('nodeconductor_digitalocean', '0002_auto_20170210_1345'),
+        ('nodeconductor_digitalocean', '0001_initial'),
     ]
 
     operations = [
